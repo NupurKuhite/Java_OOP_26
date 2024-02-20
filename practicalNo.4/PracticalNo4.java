9e5302dd1b01319f260ae461da4a17b24e593e8f
NupurKuhite <147261653+NupurKuhite@users.noreply.github.com>
2024-02-20 19:08:36 +0530

    Update PracticalNo4.java

--- practicalNo.4/PracticalNo4.java
+++ practicalNo.4/PracticalNo4.java
@@ -1,10 +1,7 @@
-/*Aim-Find the method printName. Notice the comment lines showing where you should put
-your code. This has a name passed into it with the format “<first name> <last name>”.
+/*Aim-Find the method printName. Notice the comment lines showing where you should put your code. This has a name passed into it with the format “<first name> <last name>”.
 You will reformat it to be “<last name>, <first name> and print it out.
-Example:
-“Bill Joy” becomes “Joy,Bill”
-There will always be at least one space between the first and the last name. There may be
-extra spaces between the first and last names and extra spaces at the beginning and end of
+Example: “Bill Joy” becomes “Joy,Bill”
+There will always be at least one space between the first and the last name. There may be extra spaces between the first and last names and extra spaces at the beginning and end of
 the name passed in. You will have to strip these out if they are there.
 
 Author- Nupur Kuhite
